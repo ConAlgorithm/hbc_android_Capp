be6545e44c1962351062df02a059d5330271ffd9
liuchao <chaoliu@huangbaoche.com>
2016-03-07 19:09:43 +0800

    add 图片资源

--- app/src/main/java/com/hugboga/custom/MainActivity.java
+++ app/src/main/java/com/hugboga/custom/MainActivity.java
@@ -2,7 +2,6 @@ package com.hugboga.custom;
 
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
-NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
