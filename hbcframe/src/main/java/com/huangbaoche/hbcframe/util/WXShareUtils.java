d245dae6a7fbe6f1a5d0ed3788e5cff51f4ffceb
李伟 <weili@huangbaoche.com>
2016-03-23 15:14:43 +0800

    修改微信分享图片 目前png图片可以分享成功 jpg格式分享会失败

--- hbcframe/src/main/java/com/huangbaoche/hbcframe/util/WXShareUtils.java
+++ hbcframe/src/main/java/com/huangbaoche/hbcframe/util/WXShareUtils.java
@@ -1,5 +1,6 @@
 package com.huangbaoche.hbcframe.util;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.graphics.Bitmap;
@@ -8,8 +9,10 @@ import android.os.AsyncTask;
 import android.widget.Toast;
 
 import com.huangbaoche.hbcframe.HbcConfig;
+import com.huangbaoche.hbcframe.R;
 import com.tencent.mm.sdk.modelmsg.SendMessageToWX;
 import com.tencent.mm.sdk.modelmsg.WXMediaMessage;
+import com.tencent.mm.sdk.modelmsg.WXTextObject;
 import com.tencent.mm.sdk.modelmsg.WXWebpageObject;
 import com.tencent.mm.sdk.openapi.IWXAPI;
 import com.tencent.mm.sdk.openapi.WXAPIFactory;
@@ -18,8 +21,10 @@ import org.xutils.common.Callback;
 import org.xutils.image.ImageOptions;
 import org.xutils.x;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.net.URLEncoder;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 微信分享相关操作
@@ -77,13 +82,15 @@ public class WXShareUtils {
      */
     public void share(final int type, final String picUrl, final String title, final String content, final String goUrl) {
         if (isInstall()) {
+            MLog.e("分享picUrl = "+picUrl);
             x.image().loadFile(picUrl, null, new Callback.CacheCallback<File>() {
                 @Override
                 public boolean onCache(File result) {
                     BitmapFactory.Options options = new BitmapFactory.Options();
                     options.inSampleSize = 2;
-                    Bitmap bitmap= BitmapFactory.decodeFile(result.getAbsolutePath(), options);
+                    Bitmap bitmap= BitmapFactory.decodeFile(result.getAbsolutePath(), null);
                     share(type,bitmap,title,content,goUrl);
+                    MLog.e("onCache = " + result.getAbsolutePath());
                     return true;
                 }
 
@@ -91,12 +98,14 @@ public class WXShareUtils {
                 public void onSuccess(File result) {
                     BitmapFactory.Options options = new BitmapFactory.Options();
                     options.inSampleSize = 2;
-                    Bitmap bitmap= BitmapFactory.decodeFile(result.getAbsolutePath(), options);
+                    Bitmap bitmap= BitmapFactory.decodeFile(result.getPath(), null);
                     share(type,bitmap,title,content,goUrl);
+                    MLog.e("onSuccess = " + picUrl);
                 }
 
                 @Override
                 public void onError(Throwable ex, boolean isOnCallback) {
+                    MLog.e("图片加载失败",ex);
                     if(mContext!=null)
                     Toast.makeText(mContext,"图片加载失败",Toast.LENGTH_LONG).show();
                 }
@@ -114,26 +123,42 @@ public class WXShareUtils {
                 });
 
         }else{
-            Toast.makeText(mContext,"未安装微信",Toast.LENGTH_LONG).show();
+            Toast.makeText(mContext, "未安装微信", Toast.LENGTH_LONG).show();
         }
     }
 
 
     public void share(final int type, final Bitmap bitmap, final String title, final String content, final String goUrl) {
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inSampleSize = 2;
         WXWebpageObject webpage = new WXWebpageObject();
+        MLog.e("goUrl="+goUrl);
         webpage.webpageUrl = goUrl;
         WXMediaMessage msg = new WXMediaMessage(webpage);
         msg.title = title;
         msg.description = content;
-        msg.setThumbImage(bitmap);
+        msg.thumbData =  bmpToByteArray(bitmap, true);
 
         SendMessageToWX.Req req = new SendMessageToWX.Req();
         req.transaction = String.valueOf(System.currentTimeMillis());
         req.message = msg;
         req.scene = type == TYPE_SESSION ? SendMessageToWX.Req.WXSceneSession : SendMessageToWX.Req.WXSceneTimeline;
         iwxapi.sendReq(req);
+        MLog.e("share sendReq= "+msg );
     }
 
+    public static byte[] bmpToByteArray(final Bitmap bmp, final boolean needRecycle) {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.PNG, 100, output);
+        if (needRecycle) {
+            bmp.recycle();
+        }
+
+        byte[] result = output.toByteArray();
+        try {
+            output.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
 }
