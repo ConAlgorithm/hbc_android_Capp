b8516912a0e989c440e2a0cd03ab9d5c57b71980
dyt <dingyatao@soyoung.com>
2016-04-01 17:41:02 +0800

    修改bug 城市首页，SKU列表显示错乱，另外，线路名称和行程亮点超过一行请用“...”截断

--- app/src/main/java/com/hugboga/custom/adapter/SkuAdapter.java
+++ app/src/main/java/com/hugboga/custom/adapter/SkuAdapter.java
@@ -1,6 +1,7 @@
 package com.hugboga.custom.adapter;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,9 +9,13 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.huangbaoche.hbcframe.adapter.BaseAdapter;
+import com.huangbaoche.hbcframe.data.net.DefaultImageCallback;
+import com.huangbaoche.hbcframe.util.MLog;
 import com.hugboga.custom.R;
 import com.hugboga.custom.data.bean.SkuItemBean;
+import com.hugboga.custom.widget.STGVImageView;
 
+import org.w3c.dom.Text;
 import org.xutils.image.ImageOptions;
 import org.xutils.x;
 
@@ -28,10 +33,9 @@ public class SkuAdapter extends BaseAdapter<SkuItemBean> {
                 .setLoadingDrawableId(R.mipmap.img_undertext)
                 .build();
     }
-
+    ViewHolder viewHolder = null;
     @Override
     public View getView(int position, View view, ViewGroup parent) {
-        ViewHolder viewHolder = null;
         if (view == null) {
             viewHolder = new ViewHolder();
             view = LayoutInflater.from(mContext).inflate(R.layout.item_city_sku, null);
@@ -40,12 +44,18 @@ public class SkuAdapter extends BaseAdapter<SkuItemBean> {
             viewHolder.tvGuide = (TextView) view.findViewById(R.id.item_sku_guide_number);
             viewHolder.tvAmount = (TextView) view.findViewById(R.id.item_sku_amount);
             viewHolder.tvSale = (TextView) view.findViewById(R.id.item_sku_sale);
-            viewHolder.imgBg = (ImageView) view.findViewById(R.id.item_city_sku_img);
+            viewHolder.imgBg = (STGVImageView) view.findViewById(R.id.item_city_sku_img);
+            viewHolder.top_line = (TextView) view.findViewById(R.id.top_line);
             view.setTag(viewHolder);
         } else {
             viewHolder = (ViewHolder) view.getTag();
         }
         SkuItemBean bean = getItem(position);
+        if(position == 0){
+            viewHolder.top_line.setVisibility(View.GONE);
+        }else{
+            viewHolder.top_line.setVisibility(View.VISIBLE);
+        }
         if (bean != null) {
             viewHolder.tvTitle.setText(bean.goodsName);
             viewHolder.tvLabel.setText(bean.salePoints);
@@ -54,19 +64,33 @@ public class SkuAdapter extends BaseAdapter<SkuItemBean> {
             viewHolder.tvSale.setText(mContext.getString(R.string.sku_sale_number, bean.saleAmount));
             viewHolder.tvGuide.setVisibility(bean.guideAmount == 0 ? View.GONE : View.VISIBLE);
             viewHolder.tvSale.setVisibility(bean.saleAmount == 0 ? View.GONE : View.VISIBLE);
-            x.image().bind(viewHolder.imgBg, bean.goodsPicture, options);
+
+//            x.image().bind(viewHolder.imgBg, bean.goodsPicture, options);
+
+            x.image().bind(viewHolder.imgBg, bean.goodsPicture, options,new DefaultImageCallback<Drawable>() {
+                @Override
+                public void onSuccess(Drawable result) {
+                    int height = result.getIntrinsicHeight();
+                    int width = result.getIntrinsicWidth();
+                    MLog.e(""+height+"  "+width);
+                    viewHolder.imgBg.mHeight = height;
+                    viewHolder.imgBg.mWidth = width;
+                    viewHolder.imgBg.setImageDrawable(result);
+                }
+            });
         }
 
         return view;
     }
 
     final static class ViewHolder {
-        ImageView imgBg;
+        STGVImageView imgBg;
         TextView tvTitle;
         TextView tvLabel;
         TextView tvGuide;
         TextView tvAmount;
         TextView tvSale;
+        TextView top_line;
 
     }
 
