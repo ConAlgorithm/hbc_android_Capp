6fe07da3c180c1409725c0b10f43cf2c2916a0bc
dyt <dingyatao@soyoung.com>
2016-04-16 21:14:12 +0800

    车辆信息

---
+++ app/src/main/java/com/hugboga/custom/data/bean/CarInfoBean.java
@@ -0,0 +1,21 @@
+package com.hugboga.custom.data.bean;
+
+import java.util.List;
+
+/**
+ * Created by dyt on 16/4/16.
+ */
+public class CarInfoBean {
+
+    public List<SelectCarBean> cars;
+    public int enableLocal;
+    public int guideFloatSwitch;
+//    public int guideFloats: [ ],
+    public int intownKms;
+    public int outtownKms;
+    public int overChargePerKm;
+    public int serviceHours;
+    public int stayCharge;
+    public boolean supportChildseat;
+    public int timeNotReachFlag;
+}
