895f730aec938f5e6e09a5f776bed5c37616998b
dyt <dingyatao@soyoung.com>
2016-04-18 20:35:11 +0800

    分享修改

--- hbcframe/src/main/java/com/huangbaoche/hbcframe/util/WXShareUtils.java
+++ hbcframe/src/main/java/com/huangbaoche/hbcframe/util/WXShareUtils.java
@@ -4,7 +4,8 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
+import android.net.Uri;
+
 import android.widget.Toast;
 
 import com.huangbaoche.hbcframe.HbcConfig;
@@ -13,13 +14,14 @@ import com.tencent.mm.sdk.modelmsg.WXMediaMessage;
 import com.tencent.mm.sdk.modelmsg.WXWebpageObject;
 import com.tencent.mm.sdk.openapi.IWXAPI;
 import com.tencent.mm.sdk.openapi.WXAPIFactory;
+import com.thin.downloadmanager.DefaultRetryPolicy;
+import com.thin.downloadmanager.DownloadRequest;
+import com.thin.downloadmanager.DownloadStatusListener;
+import com.thin.downloadmanager.ThinDownloadManager;
 
-import org.xutils.common.Callback;
-import org.xutils.image.ImageOptions;
-import org.xutils.x;
 
-import java.io.File;
-import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * 微信分享相关操作
@@ -71,6 +73,12 @@ public class WXShareUtils {
         return true;
     }
 
+    public static String getPhotoFileName() {
+        Date date = new Date(System.currentTimeMillis());
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
+        return dateFormat.format(date) + ".jpg";
+    }
+
     /**
      * 微信分享
      *@param type (0分享到微信好友,1分享到微信朋友圈)
@@ -82,42 +90,78 @@ public class WXShareUtils {
     public void share(final int type, final String picUrl, final String title, final String content, final String goUrl) {
         MLog.e("cache type="+type+" bitmap="+picUrl+" title="+title+" content="+content+" goUrl="+goUrl);
         if (isInstall(true)) {
-            x.image().loadFile(picUrl, null, new Callback.CacheCallback<File>() {
-                @Override
-                public boolean onCache(File result) {
-                    BitmapFactory.Options options = new BitmapFactory.Options();
-                    options.inSampleSize = 2;
-                    Bitmap bitmap= BitmapFactory.decodeFile(result.getAbsolutePath(), options);
+            Uri downloadUri = Uri.parse(picUrl);
+            final Uri destinationUri = Uri.parse(mContext.getExternalCacheDir().toString() + getPhotoFileName());
+            DownloadRequest downloadRequest = new DownloadRequest(downloadUri)
+                    .addCustomHeader("Auth-Token", "YourTokenApiKey")
+                    .setRetryPolicy(new DefaultRetryPolicy())
+                    .setDestinationURI(destinationUri).setPriority(DownloadRequest.Priority.HIGH)
+                    .setDownloadListener(new DownloadStatusListener() {
+                        @Override
+                        public void onDownloadComplete(int id) {
+                            MLog.e("onSuccess=====1111==c");
+                            BitmapFactory.Options options = new BitmapFactory.Options();
+                            options.inSampleSize = 2;
+                            try {
+                                Bitmap bitmap = BitmapFactory.decodeFile(destinationUri.getPath(), options);
+                                share(type, bitmap, title, content, goUrl);
+                            }catch (Exception e){
+                                e.printStackTrace();
+                            }
+                        }
 
-                    share(type,bitmap,title,content,goUrl);
-                    return true;
-                }
+                        @Override
+                        public void onDownloadFailed(int id, int errorCode, String errorMessage) {
 
-                @Override
-                public void onSuccess(File result) {
-                    BitmapFactory.Options options = new BitmapFactory.Options();
-                    options.inSampleSize = 2;
-                    Bitmap bitmap= BitmapFactory.decodeFile(result.getAbsolutePath(), options);
-                    share(type,bitmap,title,content,goUrl);
-                }
+                        }
 
-                @Override
-                public void onError(Throwable ex, boolean isOnCallback) {
-                    if(mContext!=null)
-                    Toast.makeText(mContext,"图片加载失败",Toast.LENGTH_LONG).show();
-                }
+                        @Override
+                        public void onProgress(int id, long totalBytes, long downlaodedBytes, int progress){
 
-                @Override
-                public void onCancelled(CancelledException cex) {
 
-                }
+                        }
+                    });
 
-                @Override
-                public void onFinished() {
+            ThinDownloadManager downloadManager;
+            downloadManager = new ThinDownloadManager();
+            downloadManager.add(downloadRequest);
 
-                }
-
-                });
+//            x.image().loadFile(picUrl, null, new Callback.CacheCallback<File>() {
+//                @Override
+//                public boolean onCache(File result) {
+//                    BitmapFactory.Options options = new BitmapFactory.Options();
+//                    options.inSampleSize = 2;
+//                    Bitmap bitmap= BitmapFactory.decodeFile(result.getAbsolutePath(), options);
+//
+//                    share(type,bitmap,title,content,goUrl);
+//                    return true;
+//                }
+//
+//                @Override
+//                public void onSuccess(File result) {
+//                    BitmapFactory.Options options = new BitmapFactory.Options();
+//                    options.inSampleSize = 2;
+//                    Bitmap bitmap= BitmapFactory.decodeFile(result.getAbsolutePath(), options);
+//                    share(type,bitmap,title,content,goUrl);
+//                }
+//
+//                @Override
+//                public void onError(Throwable ex, boolean isOnCallback) {
+//                    if(mContext!=null)
+//                    Toast.makeText(mContext,"图片加载失败",Toast.LENGTH_LONG).show();
+//                }
+//
+//                @Override
+//                public void onCancelled(CancelledException cex) {
+//
+//                }
+//
+//                @Override
+//                public void onFinished() {
+//
+//                }
+//
+//                });
 
         }else{
             Toast.makeText(mContext,"未安装微信",Toast.LENGTH_LONG).show();
