740ce93299809220c3f32abf353d7ace17656a8d
dyt <dingyatao@soyoung.com>
2016-05-17 16:53:33 +0800

    友盟升级

--- app/src/main/java/com/hugboga/custom/fragment/FgSendNew.java
+++ app/src/main/java/com/hugboga/custom/fragment/FgSendNew.java
@@ -7,10 +7,19 @@ import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.huangbaoche.hbcframe.util.MLog;
 import com.hugboga.custom.R;
+import com.hugboga.custom.data.bean.AirPort;
+import com.hugboga.custom.data.bean.PoiBean;
+import com.hugboga.custom.utils.ToastUtils;
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+import com.wdullaer.materialdatetimepicker.time.RadialPickerLayout;
+import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
 
 import org.xutils.common.Callback;
 import org.xutils.view.annotation.ContentView;
+
+import java.util.Calendar;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -45,6 +54,12 @@ public class FgSendNew extends BaseFragment {
     @Bind(R.id.rl_starttime)
     RelativeLayout rlStarttime;
 
+    private AirPort airPortBean;//航班信息
+    private PoiBean poiBean;//达到目的地
+    private String serverDate;
+    private String serverTime;
+
+
     @Override
     protected void initHeader() {
 
@@ -67,7 +82,6 @@ public class FgSendNew extends BaseFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // TODO: inflate a fragment view
         View rootView = super.onCreateView(inflater, container, savedInstanceState);
         ButterKnife.bind(this, rootView);
         return rootView;
@@ -83,23 +97,107 @@ public class FgSendNew extends BaseFragment {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.info_tips:
-                break;
             case R.id.air_title:
-                break;
             case R.id.air_detail:
+            case R.id.rl_info://从哪里出发
+                if (airPortBean != null) {
+                    FgPoiSearch fg = new FgPoiSearch();
+                    Bundle bundle = new Bundle();
+                    bundle.putInt(FgPoiSearch.KEY_CITY_ID, airPortBean.cityId);
+                    bundle.putString(FgPoiSearch.KEY_LOCATION, airPortBean.location);
+                    fg.setArguments(bundle);
+                    startFragment(fg);
+                }else {
+                    ToastUtils.showShort("先选择机场");
+                }
                 break;
-            case R.id.rl_info:
-                break;
-            case R.id.address_tips:
+            case R.id.address_tips://选择机场
+                startFragment(new FgChooseAirport());
                 break;
             case R.id.rl_address:
                 FgChooseAir fgChooseAir = new FgChooseAir();
                 startFragment(fgChooseAir);
                 break;
-            case R.id.time_text:
+            case R.id.time_text://出发时间
+                if (airPortBean == null) {
+                    ToastUtils.showShort("先选择机场");
+                    return;
+                }
+                showDaySelect();
                 break;
             case R.id.rl_starttime:
                 break;
+        }
+    }
+
+    @Override
+    public void onFragmentResult(Bundle bundle) {
+        MLog.w(this + " onFragmentResult " + bundle);
+        String from = bundle.getString(KEY_FRAGMENT_NAME);
+        if (FgChooseAirport.class.getSimpleName().equals(from)) {
+            airPortBean = (AirPort) bundle.getSerializable(FgChooseAirport.KEY_AIRPORT);
+            addressTips.setText(airPortBean.cityName + " " + airPortBean.airportName);
+            poiBean = null;
+            infoTips.setVisibility(View.VISIBLE);
+            airTitle.setVisibility(View.GONE);
+            airDetail.setVisibility(View.GONE);
+        } else if (FgPoiSearch.class.getSimpleName().equals(from)) {
+            poiBean = (PoiBean) bundle.getSerializable("arrival");
+            infoTips.setVisibility(View.GONE);
+            airTitle.setVisibility(View.VISIBLE);
+            airDetail.setVisibility(View.VISIBLE);
+            airTitle.setText(poiBean.placeName);
+            airDetail.setText(poiBean.placeDetail);
+            collapseSoftInputMethod();
+        }
+    }
+
+    public void showDaySelect() {
+        Calendar cal = Calendar.getInstance();
+        MyDatePickerListener myDatePickerDialog = new MyDatePickerListener(timeText);
+        DatePickerDialog dpd = DatePickerDialog.newInstance(
+                myDatePickerDialog, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH));
+        cal = Calendar.getInstance();
+        dpd.setMinDate(cal);
+        cal = Calendar.getInstance();
+        cal.set(Calendar.MONTH, cal.get(Calendar.MONTH) + 6);
+        dpd.setMaxDate(cal);
+        dpd.show(this.getActivity().getFragmentManager(), "DatePickerDialog");   //显示日期设置对话框
+
+    }
+
+    class MyDatePickerListener implements DatePickerDialog.OnDateSetListener {
+        TextView mTextView;
+
+        MyDatePickerListener(TextView textView) {
+            this.mTextView = textView;
+        }
+
+        @Override
+        public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+            int month = monthOfYear + 1;
+            String monthStr = String.format("%02d", month);
+            String dayOfMonthStr = String.format("%02d", dayOfMonth);
+            serverDate = year + "-" + monthStr + "-" + dayOfMonthStr;
+            showTimeSelect();
+        }
+    }
+
+    public void showTimeSelect() {
+        Calendar cal = Calendar.getInstance();
+        MyTimePickerDialogListener myTimePickerDialog = new MyTimePickerDialogListener();
+        TimePickerDialog datePickerDialog = TimePickerDialog.newInstance(myTimePickerDialog, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true);
+        datePickerDialog.show(this.getActivity().getFragmentManager(), "TimePickerDialog");                //显示日期设置对话框
+    }
+
+
+    class MyTimePickerDialogListener implements TimePickerDialog.OnTimeSetListener {
+        @Override
+        public void onTimeSet(RadialPickerLayout view, int hourOfDay, int minute, int second) {
+            String hour = String.format("%02d", hourOfDay);
+            String minuteStr = String.format("%02d", minute);
+            serverTime = hour + ":" + minuteStr;
+            timeText.setText(serverDate + " " + serverTime);
         }
     }
 }
