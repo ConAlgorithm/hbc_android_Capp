2ac86dea4522bd5f1499aeca551c4f5384c85966
jingliang <jingliangxu@huangbaoche.com>
2016-05-31 13:57:54 +0800

    add FgChoosePayment. fragment

--- app/src/main/java/com/hugboga/custom/fragment/FgChoosePayment.java
+++ app/src/main/java/com/hugboga/custom/fragment/FgChoosePayment.java
@@ -1,15 +1,24 @@
 package com.hugboga.custom.fragment;
 
+import android.view.View;
+import android.widget.TextView;
+
 import com.hugboga.custom.R;
 
 import org.xutils.common.Callback;
 import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.Event;
+import org.xutils.view.annotation.ViewInject;
 
 /**
  * Created by qingcha on 16/5/31.
  */
 @ContentView(R.layout.fg_choose_payment)
 public class FgChoosePayment extends BaseFragment {
+
+    @ViewInject(R.id.choose_payment_price_tv)
+    TextView priceTV;
+
 
     @Override
     protected void initHeader() {
@@ -29,5 +38,15 @@ public class FgChoosePayment extends BaseFragment {
     @Override
     protected void inflateContent() {
 
+    }
+
+    @Event({R.id.choose_payment_alipay_layout, R.id.choose_payment_wechat_layout,})
+    private void onClickView(View view) {
+        switch (view.getId()) {
+            case R.id.choose_payment_alipay_layout:
+                break;
+            case R.id.choose_payment_wechat_layout:
+                break;
+        }
     }
 }
