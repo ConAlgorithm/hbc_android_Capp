ca6bb2c9d5ce4cf00cd35f42cc3c65bbd11fedd2
dyt <dingyatao@soyoung.com>
2016-06-13 17:09:42 +0800

    修改BUG

--- app/src/main/java/com/hugboga/custom/fragment/FgSkuNew.java
+++ app/src/main/java/com/hugboga/custom/fragment/FgSkuNew.java
@@ -278,7 +278,7 @@ public class FgSkuNew extends BaseFragment {
         if (getArguments() != null) {
             bundle.putAll(getArguments());
         }
-        bundle.putParcelable("carListBean", carListBean);
+        bundle.putSerializable("carListBean", carListBean);
         fgCarNew.setArguments(bundle);
         transaction.add(R.id.show_cars_layout_sku, fgCarNew);
         transaction.commit();
