2719951a3a5916125baaccb95636837fe1bc5f79
jingliang <jingliangxu@huangbaoche.com>
2016-06-30 19:41:30 +0800

    tagGroup view bug fix

--- app/src/main/java/com/hugboga/custom/widget/SkuListItem.java
+++ app/src/main/java/com/hugboga/custom/widget/SkuListItem.java
@@ -113,27 +113,22 @@ public class SkuListItem extends RelativeLayout implements HbcViewBehavior{
                 if (lablesBean == null) {
                     continue;
                 }
-//                if (i < tagGroup.getChildCount()) {
-//                    LinearLayout tagLayout = (LinearLayout)tagGroup.getChildAt(i);
-//                    tagLayout.setVisibility(View.VISIBLE);
-//                    ImageView iconIV = (ImageView)tagLayout.getChildAt(0);
-//                    iconIV.setBackgroundResource(getLableIconRes(skuItemBean.goodsClass, lablesBean.lableType));
-//                    TextView tagTV = (TextView)tagLayout.getChildAt(1);
-//                    tagTV.setText(lablesBean.lableName);
-//                    tagTV.setTextColor(getTagColor(skuItemBean.goodsClass));
-//                } else {
+                if (i < tagGroup.getChildCount()) {
+                    LinearLayout tagLayout = (LinearLayout)tagGroup.getChildAt(i);
+                    tagLayout.setVisibility(View.VISIBLE);
+                    ImageView iconIV = (ImageView)tagLayout.getChildAt(0);
+                    iconIV.setBackgroundResource(getLableIconRes(skuItemBean.goodsClass, lablesBean.lableType));
+                    TextView tagTV = (TextView)tagLayout.getChildAt(1);
+                    tagTV.setText(lablesBean.lableName);
+                    tagTV.setTextColor(getTagColor(skuItemBean.goodsClass));
+                } else {
                     viewList.add(getNewTagView(lablesBean.lableName, lablesBean.lableType, skuItemBean.goodsClass));
-//                }
+                }
             }
-//            if (tagGroup.getChildCount() != 0 && tagGroup.getChildCount() > labelsSize) {
-//                for (int j = labelsSize; labelsSize < tagGroup.getChildCount(); j++) {
-//                    if (tagGroup.getChildAt(j-1) != null) {
-//                        Log.i("aa", " tagGroup.getChildCount() "+tagGroup.getChildCount()+  " labelsSize "+labelsSize + " --j "+j + " skuItemBean.goodsNo "+skuItemBean.goodsNo);
-//                        tagGroup.getChildAt(j-1).setVisibility(View.GONE);
-//                    }
-//                }
-//            }
-            tagGroup.setTags(viewList, true);//tagGroup.getChildCount() <= 0
+            for (int j = labelsSize; j < tagGroup.getChildCount(); j++) {
+                tagGroup.getChildAt(j).setVisibility(View.GONE);
+            }
+            tagGroup.setTags(viewList, tagGroup.getChildCount() <= 0);
         } else {
             tagGroup.setVisibility(View.GONE);
         }
