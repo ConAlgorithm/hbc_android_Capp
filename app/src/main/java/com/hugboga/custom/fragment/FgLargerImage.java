5e580cbf1b29bb8735adc8bc5fbaed74a71405a6
jingliang <jingliangxu@huangbaoche.com>
2016-07-08 15:56:48 +0800

    IM update

--- app/src/main/java/com/hugboga/custom/fragment/FgLargerImage.java
+++ app/src/main/java/com/hugboga/custom/fragment/FgLargerImage.java
@@ -20,7 +20,7 @@ import org.xutils.view.annotation.ViewInject;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-import uk.co.senab.photoview.PhotoView;
+import io.rong.photoview.PhotoView;
 
 
 /**
