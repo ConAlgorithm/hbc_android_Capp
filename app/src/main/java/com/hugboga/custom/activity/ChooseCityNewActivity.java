178431c14c88832ef0fd554637559349ecdd935f
dyt <dingyatao@soyoung.com>
2016-08-04 20:45:12 +0800

    框架调整

--- app/src/main/java/com/hugboga/custom/activity/ChooseCityNewActivity.java
+++ app/src/main/java/com/hugboga/custom/activity/ChooseCityNewActivity.java
@@ -158,7 +158,9 @@ public class ChooseCityNewActivity extends BaseActivity {
             case R.id.header_left_btn:
                 expandableListView.setVisibility(View.GONE);
                 finish();
-                overridePendingTransition(R.anim.push_buttom_out,0);
+                if(!isHomeIn) {
+                    overridePendingTransition(R.anim.push_buttom_out, 0);
+                }
                 break;
             case R.id.head_search_clean:
                 headSearch.setText("");
