e7c154a8cced9f1c2c12aa98d80697e97d39ee8f
dyt <dingyatao@soyoung.com>
2016-08-06 14:40:48 +0800

    框架调整

--- app/src/main/java/com/hugboga/custom/data/bean/InsureResultBean.java
+++ app/src/main/java/com/hugboga/custom/data/bean/InsureResultBean.java
@@ -6,7 +6,7 @@ import android.os.Parcelable;
 /**
  * Created  on 16/4/23.
  */
-public class InsureResultBean implements Parcelable{
+public class InsureResultBean implements IBaseBean{
 //    {"birthday":"2013-10-10","insuranceUserId":"IU91440316911","name":"李达","passportNo":"G123","sex":1,"userId":"123","userStatus":1},
 
     public String birthday;
@@ -51,50 +51,5 @@ public class InsureResultBean implements Parcelable{
         return result;
     }
 
-    @Override
-    public int describeContents() {
-        return 0;
-    }
 
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(this.birthday);
-        dest.writeString(this.insuranceUserId);
-        dest.writeString(this.name);
-        dest.writeString(this.passportNo);
-        dest.writeInt(this.sex);
-        dest.writeString(this.userId);
-        dest.writeInt(this.userStatus);
-        dest.writeInt(this.intsource);
-        dest.writeInt(this.isDel);
-        dest.writeInt(this.isCheck);
-    }
-
-    public InsureResultBean() {
-    }
-
-    protected InsureResultBean(Parcel in) {
-        this.birthday = in.readString();
-        this.insuranceUserId = in.readString();
-        this.name = in.readString();
-        this.passportNo = in.readString();
-        this.sex = in.readInt();
-        this.userId = in.readString();
-        this.userStatus = in.readInt();
-        this.intsource = in.readInt();
-        this.isDel = in.readInt();
-        this.isCheck = in.readInt();
-    }
-
-    public static final Creator<InsureResultBean> CREATOR = new Creator<InsureResultBean>() {
-        @Override
-        public InsureResultBean createFromParcel(Parcel source) {
-            return new InsureResultBean(source);
-        }
-
-        @Override
-        public InsureResultBean[] newArray(int size) {
-            return new InsureResultBean[size];
-        }
-    };
 }
