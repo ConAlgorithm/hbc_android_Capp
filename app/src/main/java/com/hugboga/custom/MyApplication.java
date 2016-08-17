b47b56f90489b3702f540e62f1956aa3896d81cb
dyt <dingyatao@soyoung.com>
2016-08-17 17:39:50 +0800

    2.9.0封包

--- app/src/main/java/com/hugboga/custom/MyApplication.java
+++ app/src/main/java/com/hugboga/custom/MyApplication.java
@@ -16,6 +16,7 @@ import com.hugboga.custom.data.net.UrlLibs;
 import com.hugboga.custom.data.request.RequestAccessKey;
 import com.hugboga.custom.utils.LogUtils;
 import com.hugboga.custom.widget.DialogUtil;
+import com.tencent.bugly.crashreport.CrashReport;
 import com.umeng.analytics.MobclickAgent;
 
 import org.xutils.x;
@@ -43,7 +44,7 @@ public class MyApplication extends HbcApplication {
         mAppContext = this.getApplicationContext();
         Log.e("hbcApplication", "debug " + BuildConfig.DEBUG);
         try {
-//            CrashReport.initCrashReport(this, "900024779", false);
+            CrashReport.initCrashReport(this, "900024779", false);
             Reservoir.init(this, 4096);
         } catch (Exception e) {
             e.printStackTrace();
