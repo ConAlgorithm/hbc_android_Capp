c0e19a7dba33d876829759299a300ec6f5ff0ef3
jingliang <jingliangxu@huangbaoche.com>
2016-08-19 20:00:32 +0800

    统计埋点

---
+++ app/src/main/java/com/hugboga/custom/statistic/event/EventPaySuccess.java
@@ -0,0 +1,11 @@
+package com.hugboga.custom.statistic.event;
+
+/**
+ * Created by qingcha on 16/8/19.
+ */
+public class EventPaySuccess extends EventBase{
+    @Override
+    public String getEventId() {
+        return null;
+    }
+}
