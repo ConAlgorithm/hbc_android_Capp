e4f1627a12f6c2974f04ebd30422967b2830791a
jingliang <jingliangxu@huangbaoche.com>
2016-11-18 20:39:02 +0800

    v3.1 旅游基金文案接口返回

--- app/src/main/java/com/hugboga/custom/data/bean/TravelFundData.java
+++ app/src/main/java/com/hugboga/custom/data/bean/TravelFundData.java
@@ -142,6 +142,10 @@ public class TravelFundData implements Serializable {
         private String couponAmount;         // 600元
         private String rewardAmountPerOrder; // 30元
         private String rewardRatePerOrder;   // 5%
+        public String headline;              // 标题
+        public String subhead;               // 副标题
+        public String name;                  // 名字
+        public String nickName;              // 昵称
 
         public String getRewardRatePerOrder() {
             return rewardRatePerOrder;
