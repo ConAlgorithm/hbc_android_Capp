61d14e408b9e4cde53b2a85c2a3774c5fea14011
jingliang <jingliangxu@huangbaoche.com>
2017-01-03 12:05:33 +0800

    司导派单BUG fix

--- app/src/main/java/com/hugboga/custom/data/bean/DeliverInfoBean.java
+++ app/src/main/java/com/hugboga/custom/data/bean/DeliverInfoBean.java
@@ -4,23 +4,28 @@ import java.io.Serializable;
 
 /**
  * Created by qingcha on 16/9/8.
+ * http://wiki.hbc.tech/pages/viewpage.action?pageId=6619604
  */
 public class DeliverInfoBean implements Serializable{
 
     public String deliverMessage;
     public String deliverDetail;
+
     /**
      * 1:未发单；2:已发单；3-已通知司导；4-有司导表态；5-正在安排司导；6-已通知该司导；7-为您协调司导；8-已确定导游；9-发单中
      * */
     public int deliverStatus;
+
     /**
      * 时间戳 毫秒
      * */
     public int span;
+
     /**
      * 发单间隔
      * */
     public int deliverTimeSpan;
+
     /**
      * 倒计时总时间 毫秒
      * */
@@ -31,8 +36,17 @@ public class DeliverInfoBean implements Serializable{
      * */
     private int canChoose;
 
+    /**
+     * 支付成功2001 其他状态都不显示发单详情
+     **/
+    private int orderStatus;
+
     public boolean isCanChoose() {
         return canChoose == 1;
+    }
+
+    public boolean isOrderStatusChanged() {
+        return orderStatus != 2001;
     }
 
     public static class DeliverStatus {
