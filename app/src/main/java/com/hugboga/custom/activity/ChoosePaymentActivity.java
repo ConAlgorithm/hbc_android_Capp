9490952489ee4ca50ed2de9ac9588038bd5df47c
jingliang <jingliangxu@huangbaoche.com>
2017-03-28 15:58:38 +0800

    fix some bugs

--- app/src/main/java/com/hugboga/custom/activity/ChoosePaymentActivity.java
+++ app/src/main/java/com/hugboga/custom/activity/ChoosePaymentActivity.java
@@ -510,7 +510,7 @@ public class ChoosePaymentActivity extends BaseActivity {
                 intent = new Intent(ChoosePaymentActivity.this, MainActivity.class);
                 startActivity(intent);
                 EventBus.getDefault().post(new EventAction(EventType.SET_MAIN_PAGE_INDEX, 2));
-                EventBus.getDefault().post(new EventAction(EventType.TRAVEL_LIST_TYPE, 1));
+                EventBus.getDefault().post(new EventAction(EventType.TRAVEL_LIST_TYPE, 0));
 
                 OrderDetailActivity.Params orderParams = new OrderDetailActivity.Params();
                 orderParams.orderId = requestParams.orderId;
