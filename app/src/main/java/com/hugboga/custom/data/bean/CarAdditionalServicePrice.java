f327d6b46da0abdc4a691d74bf7e0f807b1969d8
jingliang <jingliangxu@huangbaoche.com>
2017-04-26 12:58:36 +0800

    fix some bugs

--- app/src/main/java/com/hugboga/custom/data/bean/CarAdditionalServicePrice.java
+++ app/src/main/java/com/hugboga/custom/data/bean/CarAdditionalServicePrice.java
@@ -1,11 +1,9 @@
 package com.hugboga.custom.data.bean;
 
-import java.io.Serializable;
-
 /**
  * Created  on 16/5/19.
  */
-public class CarAdditionalServicePrice implements IBaseBean,Serializable {
+public class CarAdditionalServicePrice implements IBaseBean {
     public String checkInPrice;
     public String pickupSignPrice;
     public String childSeatPrice1;
