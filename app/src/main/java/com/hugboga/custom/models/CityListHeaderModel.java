a921add2c19ace569115d8ac4d4c28d683c8677b
jingliang <jingliangxu@huangbaoche.com>
2017-05-05 10:10:25 +0800

    城市页UI修改

--- app/src/main/java/com/hugboga/custom/models/CityListHeaderModel.java
+++ app/src/main/java/com/hugboga/custom/models/CityListHeaderModel.java
@@ -2,16 +2,23 @@ package com.hugboga.custom.models;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.TextUtils;
+import android.text.style.ImageSpan;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.airbnb.epoxy.EpoxyModel;
+import com.hugboga.custom.MyApplication;
 import com.hugboga.custom.R;
 import com.hugboga.custom.activity.ChooseCityNewActivity;
 import com.hugboga.custom.constants.Constants;
 import com.hugboga.custom.data.bean.CityListBean;
+import com.hugboga.custom.utils.CenterImageSpan;
 import com.hugboga.custom.utils.Tools;
 import com.hugboga.custom.utils.UIUtils;
 
@@ -34,12 +41,28 @@ public class CityListHeaderModel extends EpoxyModel<RelativeLayout> {
         ImageView bgIV = (ImageView) view.findViewById(R.id.city_list_header_bg_iv);
         TextView descriptionTV = (TextView) view.findViewById(R.id.city_list_header_description_tv);
         TextView cityTV = (TextView) view.findViewById(R.id.city_list_header_city_tv);
-        displayLayoutHeight = (int)((400 / 720.0f) * UIUtils.getScreenWidth());
+        displayLayoutHeight = (int)((500 / 750.0f) * UIUtils.getScreenWidth());
         view.getLayoutParams().height = displayLayoutHeight;
 
         Tools.showImage(bgIV, cityContent.cityHeadPicture, R.drawable.city_banner_default);
-        cityTV.setText(cityContent.cityName);
-        descriptionTV.setText(cityContent.cityDesc);
+        if (cityContent.cityName != null) {
+            String cityName = cityContent.cityName + " ";
+            Drawable drawable = MyApplication.getAppContext().getResources().getDrawable(R.mipmap.city_arrow_down);
+            drawable.setBounds(0, 0, UIUtils.dip2px(18), UIUtils.dip2px(10));
+            SpannableString spannable = new SpannableString(cityName + "[icon]");
+            CenterImageSpan span = new CenterImageSpan(drawable, ImageSpan.ALIGN_BASELINE);
+            spannable.setSpan(span, cityName.length(), cityName.length() + "[icon]".length(), Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
+            cityTV.setText(spannable);
+        } else {
+            cityTV.setText("");
+        }
+
+        if (TextUtils.isEmpty(cityContent.cityDesc)) {
+            descriptionTV.setVisibility(View.GONE);
+        } else {
+            descriptionTV.setVisibility(View.VISIBLE);
+            descriptionTV.setText(cityContent.cityDesc);
+        }
 
         cityTV.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,4 +84,5 @@ public class CityListHeaderModel extends EpoxyModel<RelativeLayout> {
     public int getDisplayLayoutHeight() {
         return displayLayoutHeight;
     }
+
 }
