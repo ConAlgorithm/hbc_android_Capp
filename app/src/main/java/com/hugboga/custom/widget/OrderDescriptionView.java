e2c2264dd567fc4f7aba2779aae972787c3023cd
jingliang <jingliangxu@huangbaoche.com>
2017-05-23 13:55:51 +0800

    确认订单 订单信息view

---
+++ app/src/main/java/com/hugboga/custom/widget/OrderDescriptionView.java
@@ -0,0 +1,110 @@
+package com.hugboga.custom.widget;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.hugboga.custom.R;
+import com.hugboga.custom.activity.OrderActivity;
+import com.hugboga.custom.data.bean.FlightBean;
+import com.hugboga.custom.utils.DateUtils;
+import com.hugboga.custom.utils.UIUtils;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * Created by qingcha on 17/5/22.
+ */
+public class OrderDescriptionView extends LinearLayout {
+
+    @Bind(R.id.order_desc_title_tv)
+    TextView titleTV;
+
+    @Bind(R.id.item_itinerary_sub_title_tv)
+    TextView timeTV;
+    @Bind(R.id.item_itinerary_sub_describe_tv)
+    TextView flightTV;
+
+    @Bind(R.id.order_travel_item_start_tv)
+    TextView startTV;
+    @Bind(R.id.order_travel_item_start_des_tv)
+    TextView startDesTV;
+    @Bind(R.id.order_travel_item_start_line_view)
+    View startLineView;
+    @Bind(R.id.order_travel_item_end_tv)
+    TextView endTV;
+    @Bind(R.id.order_travel_item_end_des_tv)
+    TextView endDesTV;
+
+    @Bind(R.id.item_itinerary_title_tv)
+    TextView carDescTV;
+
+    public OrderDescriptionView(Context context) {
+        this(context, null);
+    }
+
+    public OrderDescriptionView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        View view = inflate(context, R.layout.view_order_description, this);
+        ButterKnife.bind(view);
+        setOrientation(LinearLayout.VERTICAL);
+        flightTV.setVisibility(View.GONE);
+    }
+
+    public void setPickData(OrderActivity.Params params) {
+        titleTV.setText("接机");
+
+        FlightBean flightBean = params.flightBean;
+        setTime(flightBean.arrDate, flightBean.arrivalTime);
+        flightTV.setVisibility(View.VISIBLE);
+        flightTV.setText(String.format("航班: %1$s %2$s-%3$s",flightBean.flightNo, flightBean.depCityName, flightBean.arrCityName));
+
+        setAddress(flightBean.arrAirportName, null, params.poiBean.placeName, params.poiBean.placeDetail);
+
+        carDescTV.setText(params.carBean.carDesc);
+    }
+
+    private void setTime(String date, String time) {
+        timeTV.setText(DateUtils.getStrWeekFormat3(date) + " " + time);
+    }
+
+    private void setAddress(String startAddress, String startAddressDetail, String destAddress, String destAddressDetail) {
+        startTV.setText(startAddress);
+        if (!TextUtils.isEmpty(startAddressDetail)) {
+            startDesTV.setVisibility(View.VISIBLE);
+            startDesTV.setText(startAddressDetail);
+
+            int showWidth = UIUtils.getScreenWidth() - (getContext().getResources().getDimensionPixelOffset(R.dimen.order_padding_left) * 2 + UIUtils.dip2px(24) + UIUtils.dip2px(3));
+            int stringWidth = UIUtils.getStringWidth(startDesTV, startAddressDetail);
+            int lines = (int)Math.ceil(stringWidth / showWidth);
+            int startDesViewWidth = UIUtils.dip2px(20) + lines * UIUtils.dip2px(10);
+
+            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(UIUtils.dip2px(1), startDesViewWidth);
+            params.topMargin = UIUtils.dip2px(20);
+            params.addRule(RelativeLayout.ALIGN_BOTTOM, R.id.travel_item_start_des_tv);
+            params.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+            params.leftMargin = UIUtils.dip2px(7.5f);
+            startLineView.setLayoutParams(params);
+        } else {
+            startDesTV.setVisibility(View.GONE);
+
+            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(UIUtils.dip2px(1), UIUtils.dip2px(5));
+            params.topMargin = UIUtils.dip2px(20);
+            params.leftMargin = UIUtils.dip2px(7.5f);
+            startLineView.setLayoutParams(params);
+        }
+        endTV.setText(destAddress);
+        if (!TextUtils.isEmpty(destAddressDetail)) {
+            endDesTV.setVisibility(View.VISIBLE);
+            endDesTV.setText(destAddressDetail);
+        } else {
+            endDesTV.setVisibility(View.GONE);
+        }
+    }
+}
