c0aac821d658037a4244945879564b1cd9b121e9
zhangqiang <tjbdzq@163.com>
2017-08-15 15:51:04 +0800

    添加推荐城市跳转逻辑

--- app/src/main/java/com/hugboga/custom/adapter/HomeRecommendedRoutAdapter.java
+++ app/src/main/java/com/hugboga/custom/adapter/HomeRecommendedRoutAdapter.java
@@ -2,7 +2,6 @@ package com.hugboga.custom.adapter;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
 import android.support.v4.view.PagerAdapter;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
@@ -13,27 +12,15 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.hugboga.custom.R;
-import com.hugboga.custom.activity.CityListActivity;
-import com.hugboga.custom.activity.FilterSkuListActivity;
 import com.hugboga.custom.activity.SkuDetailActivity;
 import com.hugboga.custom.activity.WebInfoActivity;
 import com.hugboga.custom.constants.Constants;
 import com.hugboga.custom.data.bean.HomeCityContentVo2;
-import com.hugboga.custom.data.bean.HomeCityGoodsVo;
-import com.hugboga.custom.data.event.EventAction;
-import com.hugboga.custom.data.event.EventType;
-import com.hugboga.custom.statistic.MobClickUtils;
 import com.hugboga.custom.statistic.sensors.SensorsUtils;
 import com.hugboga.custom.utils.Tools;
 import com.hugboga.custom.utils.UIUtils;
 
 import net.grobas.view.PolygonImageView;
-
-import org.greenrobot.eventbus.EventBus;
-
-import java.util.ArrayList;
-
-import butterknife.Bind;
 
 /**
  * Created by zhangqiang on 17/8/3.
@@ -50,7 +37,9 @@ public class HomeRecommendedRoutAdapter extends PagerAdapter {
     TextView tiyan;
     TextView perPrice;
     PolygonImageView polygonImageView;
-    public HomeRecommendedRoutAdapter(Context context,HomeCityContentVo2 homeCityContentVo2) {
+    LinearLayout contentLayout;
+
+    public HomeRecommendedRoutAdapter(Context context, HomeCityContentVo2 homeCityContentVo2) {
         this.homeCityContentVo2 = homeCityContentVo2;
         this.context = context;
     }
@@ -69,13 +58,10 @@ public class HomeRecommendedRoutAdapter extends PagerAdapter {
     public Object instantiateItem(final ViewGroup container, final int position) {
 
         LinearLayout linearLayout = (LinearLayout) LayoutInflater.from(context).inflate(R.layout.layout_child, null);
-        //new LinearLayout(container.getContext());
         ImageView imageView = (ImageView) linearLayout.findViewById(R.id.pager_img);
-        imageView.getLayoutParams().width = UIUtils.getScreenWidth()-2*UIUtils.dip2px(16);
-        imageView.getLayoutParams().height = imageView.getLayoutParams().width * 189 /330;
-        //textView.setText(position + "");
-        //linearLayout.setId(R.id.item_id);
-        Tools.showImage(imageView,homeCityContentVo2.cityGoodsList.get(position).goodsPic);
+        imageView.getLayoutParams().width = UIUtils.getScreenWidth() - 2 * UIUtils.dip2px(16);
+        imageView.getLayoutParams().height = imageView.getLayoutParams().width * 189 / 330;
+        Tools.showImage(imageView, homeCityContentVo2.cityGoodsList.get(position).goodsPic);
 
         des1 = (TextView) linearLayout.findViewById(R.id.des1);
         cityName = (TextView) linearLayout.findViewById(R.id.cityName);
@@ -84,44 +70,41 @@ public class HomeRecommendedRoutAdapter extends PagerAdapter {
         tiyan = (TextView) linearLayout.findViewById(R.id.tiyan);
         perPrice = (TextView) linearLayout.findViewById(R.id.perPrice);
         polygonImageView = (PolygonImageView) linearLayout.findViewById(R.id.avr);
+        contentLayout = (LinearLayout) linearLayout.findViewById(R.id.content_layout);
 
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(context, SkuDetailActivity.class);
-                intent.putExtra(WebInfoActivity.WEB_URL, homeCityContentVo2.cityGoodsList.get(position).goodsDetailUrl);
-                intent.putExtra(Constants.PARAMS_ID, homeCityContentVo2.cityGoodsList.get(position).goodsNo);
-                intent.putExtra(Constants.PARAMS_SOURCE, getEventSource());
-                context.startActivity(intent);
+                interClick(position);
             }
         });
 
-        //if (homeRecommendedRouteHolder != null) {
-            des1.setText(homeCityContentVo2.cityGoodsList.get(position).goodsName);
-            cityName.setText(homeCityContentVo2.cityName + "司导推荐");
-            guidesNum.setText(homeCityContentVo2.cityGoodsList.get(position).guidesNum + "位中文司导可服务");
-            des2.setText(homeCityContentVo2.cityGoodsList.get(position).recommendedReason);
-            tiyan.setText("已体验" + homeCityContentVo2.cityGoodsList.get(position).purchases);
-            perPrice.setText("¥" + homeCityContentVo2.cityGoodsList.get(position).perPrice + "起/人");
-            if (!TextUtils.isEmpty(homeCityContentVo2.cityGoodsList.get(position).goodsPic)) {
-                Tools.showRoundImage(polygonImageView, homeCityContentVo2.cityGoodsList.get(position).guidePic, UIUtils.dip2px(5),R.mipmap.home_head_portrait);
+        des1.setText(homeCityContentVo2.cityGoodsList.get(position).goodsName);
+        cityName.setText(homeCityContentVo2.cityName + "司导推荐");
+        guidesNum.setText(homeCityContentVo2.cityGoodsList.get(position).guidesNum + "位中文司导可服务");
+        des2.setText(homeCityContentVo2.cityGoodsList.get(position).recommendedReason);
+        tiyan.setText("已体验" + homeCityContentVo2.cityGoodsList.get(position).purchases);
+        perPrice.setText("¥" + homeCityContentVo2.cityGoodsList.get(position).perPrice + "起/人");
+        if (!TextUtils.isEmpty(homeCityContentVo2.cityGoodsList.get(position).goodsPic)) {
+            Tools.showRoundImage(polygonImageView, homeCityContentVo2.cityGoodsList.get(position).guidePic, UIUtils.dip2px(5), R.mipmap.home_head_portrait);
+        }
+
+        perPrice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                interClick(position);
             }
+        });
 
-            perPrice.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(context, SkuDetailActivity.class);
-                    intent.putExtra(WebInfoActivity.WEB_URL, homeCityContentVo2.cityGoodsList.get(position).goodsDetailUrl);
-                    intent.putExtra(Constants.PARAMS_ID, homeCityContentVo2.cityGoodsList.get(position).goodsNo);
-                    intent.putExtra(Constants.PARAMS_SOURCE, getEventSource());
-                    context.startActivity(intent);
-                    SensorsUtils.onAppClick(getEventSource(),"推荐线路","首页-推荐线路");
-                }
-            });
+        contentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                interClick(position);
+            }
+        });
 
-        //}
         container.addView(linearLayout);
-        linearLayout.getLayoutParams().height = imageView.getLayoutParams().height +UIUtils.dip2px(256);
+        linearLayout.getLayoutParams().height = imageView.getLayoutParams().height + UIUtils.dip2px(256);
 //        linearLayout.getLayoutParams().width = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 180, container.getContext().getResources().getDisplayMetrics());
 //        linearLayout.getLayoutParams().height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 400, container.getContext().getResources().getDisplayMetrics());
         return linearLayout;
@@ -133,7 +116,7 @@ public class HomeRecommendedRoutAdapter extends PagerAdapter {
         container.removeView(view);
     }
 
-    public void setData(HomeCityContentVo2 homeCityContentVo2){
+    public void setData(HomeCityContentVo2 homeCityContentVo2) {
         this.homeCityContentVo2 = homeCityContentVo2;
         notifyDataSetChanged();
     }
@@ -142,13 +125,12 @@ public class HomeRecommendedRoutAdapter extends PagerAdapter {
         return "首页";
     }
 
-    private void intentActivity(Context context, Class<?> cls, String eventId, CityListActivity.Params params) {
-        Intent intent = new Intent(context, cls);
+    private void interClick(int position) {
+        Intent intent = new Intent(context, SkuDetailActivity.class);
+        intent.putExtra(WebInfoActivity.WEB_URL, homeCityContentVo2.cityGoodsList.get(position).goodsDetailUrl);
+        intent.putExtra(Constants.PARAMS_ID, homeCityContentVo2.cityGoodsList.get(position).goodsNo);
         intent.putExtra(Constants.PARAMS_SOURCE, getEventSource());
-        intent.putExtra(Constants.PARAMS_DATA, params);
         context.startActivity(intent);
-        if (!TextUtils.isEmpty(eventId)) {
-            MobClickUtils.onEvent(eventId);
-        }
+        SensorsUtils.onAppClick(getEventSource(), "推荐线路", "首页-推荐线路");
     }
 }
