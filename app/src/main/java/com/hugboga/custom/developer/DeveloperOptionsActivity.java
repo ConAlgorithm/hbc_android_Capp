bade3231c51f1a54c6fc2ffd77101fda5adb5777
jingliang <jingliangxu@huangbaoche.com>
2017-08-16 15:41:08 +0800

    开发者选项修改

--- app/src/main/java/com/hugboga/custom/developer/DeveloperOptionsActivity.java
+++ app/src/main/java/com/hugboga/custom/developer/DeveloperOptionsActivity.java
@@ -9,6 +9,7 @@ import com.hugboga.custom.action.constants.ActionPageType;
 import com.hugboga.custom.action.data.ActionBean;
 import com.hugboga.custom.action.data.ActionGuideDetailBean;
 import com.hugboga.custom.action.data.ActionOrderDetailBean;
+import com.hugboga.custom.action.data.ActionSkuDetailBean;
 import com.hugboga.custom.activity.BaseActivity;
 import com.hugboga.custom.utils.CommonUtils;
 import butterknife.OnClick;
@@ -51,6 +52,18 @@ public class DeveloperOptionsActivity extends BaseActivity{
         ActionGuideDetailBean actionGuideDetailBean = new ActionGuideDetailBean();
         actionGuideDetailBean.guideId = editText.getText().toString();
         ActionBean actionBean = new ActionBean(ActionPageType.GUIDE_DETAIL, actionGuideDetailBean, "");
+        ActionUtils.doAction(this, actionBean);
+    }
+
+    @OnClick(R.id.developer_skudetail_confirm_tv)
+    public void intentSkuDetail() {
+        EditText editText = (EditText) findViewById(R.id.developer_skudetail_et);
+        if (editText.getText() == null || editText.getText().toString() == null) {
+            CommonUtils.showToast("商品ID不能为空");
+        }
+        ActionSkuDetailBean actionSkuDetailBean = new ActionSkuDetailBean();
+        actionSkuDetailBean.goodsNo = editText.getText().toString();
+        ActionBean actionBean = new ActionBean(ActionPageType.SKU_DETAIL, actionSkuDetailBean, "");
         ActionUtils.doAction(this, actionBean);
     }
 }
