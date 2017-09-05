baa216117ae6d7c756f98cf919123da6d12eb9e3
jingliang <jingliangxu@huangbaoche.com>
2017-09-05 12:32:42 +0800

    fix some bugs

--- app/src/main/java/com/hugboga/custom/widget/CharterFirstCountView.java
+++ app/src/main/java/com/hugboga/custom/widget/CharterFirstCountView.java
@@ -215,6 +215,7 @@ public class CharterFirstCountView extends LinearLayout implements ChooseCountVi
                 hintTV.setText(contentStr);
             }
         }
+        checkCountView();
         setHintViewVisibility();
         setCountViewEnabled(true);
     }
