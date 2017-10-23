450894fd4baf844ba4d6d8e8c1e876b50987750d
zongfi <zhzephizhao@huangbaoche.com>
2017-10-23 19:45:58 +0800

    添加国内信用卡有效期弹框部分

--- app/src/main/java/com/hugboga/custom/activity/DomesticCreditCAddActivity.java
+++ app/src/main/java/com/hugboga/custom/activity/DomesticCreditCAddActivity.java
@@ -2,9 +2,14 @@ package com.hugboga.custom.activity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
 import com.hugboga.custom.R;
+import com.hugboga.custom.widget.DatePickerYearDialog;
+
+import java.util.Calendar;
 
 import butterknife.Bind;
 import butterknife.OnClick;
@@ -17,6 +22,8 @@ public class DomesticCreditCAddActivity extends BaseActivity {
 
     @Bind(R.id.header_title)
     TextView toolbarTitle;
+    @Bind(R.id.domestic_add_number2)
+    TextView tvDomesticDate; //信用卡有效期
 
     @Override
     public int getContentViewId() {
@@ -29,12 +36,46 @@ public class DomesticCreditCAddActivity extends BaseActivity {
         toolbarTitle.setText(getTitle());
     }
 
-    @OnClick({R.id.header_left_btn})
+    @OnClick({R.id.header_left_btn, R.id.domestic_layout2})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.header_left_btn:
                 finish();
                 break;
+            case R.id.domestic_layout2:
+                //选择有效期
+                Calendar calendar = Calendar.getInstance();
+                DatePickerYearDialog dialog = new DatePickerYearDialog(this, 0, new DatePickerYearDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker startDatePicker, int startYear, int startMonthOfYear, int startDayOfMonth) {
+                        tvDomesticDate.setText((startMonthOfYear + 1) + "月份／" + startYear + "年");
+                    }
+                }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+                dialog.setTitle("设置有效期");
+                dialog.show();
+                break;
         }
+    }
+
+    /**
+     * 从当前Dialog中查找DatePicker子控件
+     *
+     * @param group
+     * @return
+     */
+    private DatePicker findDatePicker(ViewGroup group) {
+        if (group != null) {
+            for (int i = 0, j = group.getChildCount(); i < j; i++) {
+                View child = group.getChildAt(i);
+                if (child instanceof DatePicker) {
+                    return (DatePicker) child;
+                } else if (child instanceof ViewGroup) {
+                    DatePicker result = findDatePicker((ViewGroup) child);
+                    if (result != null)
+                        return result;
+                }
+            }
+        }
+        return null;
     }
 }
