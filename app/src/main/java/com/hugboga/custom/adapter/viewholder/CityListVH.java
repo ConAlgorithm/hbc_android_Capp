f3ce726ff43ccb49f5032d5b5a8dc4e6e0e6c5af
zongfi <zhzephizhao@huangbaoche.com>
2017-11-27 20:55:22 +0800

    线路列表数据增加引用

--- app/src/main/java/com/hugboga/custom/adapter/viewholder/CityListVH.java
+++ app/src/main/java/com/hugboga/custom/adapter/viewholder/CityListVH.java
@@ -3,8 +3,11 @@ package com.hugboga.custom.adapter.viewholder;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.hugboga.custom.R;
+
+import org.w3c.dom.Text;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -16,6 +19,16 @@ import butterknife.ButterKnife;
 
 public class CityListVH extends RecyclerView.ViewHolder {
 
+    @BindView(R.id.city_item_img)
+    ImageView city_item_img;
+    @BindView(R.id.city_item_title)
+    TextView city_item_title; //标题
+    @BindView(R.id.city_item_guide)
+    ImageView city_item_guide; //司导头像
+    @BindView(R.id.city_item_tip)
+    TextView city_item_tip; //提示语1
+    @BindView(R.id.city_item_tip2)
+    TextView city_item_tip2; //提示语2
     @BindView(R.id.city_item_line)
     public ImageView line;
 
