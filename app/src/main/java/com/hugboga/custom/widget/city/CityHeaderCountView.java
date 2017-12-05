b60fb6b1a804571ac76487904afa77b0c5986caf
zongfi <zhzephizhao@huangbaoche.com>
2017-12-05 17:57:21 +0800

    统计信息没有玩法时的显示优化

--- app/src/main/java/com/hugboga/custom/widget/city/CityHeaderCountView.java
+++ app/src/main/java/com/hugboga/custom/widget/city/CityHeaderCountView.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.FrameLayout;
@@ -29,6 +30,10 @@ public class CityHeaderCountView extends FrameLayout {
     TextView city_header_count_pay; //包车玩法
     @BindView(R.id.city_header_count_guide)
     TextView city_header_count_guide; //司导数
+    @BindView(R.id.city_header_count2_root)
+    ConstraintLayout city_header_count2_root;
+    @BindView(R.id.city_header_count_guide2)
+    TextView city_header_count_guide2; //司导数
 
     public CityHeaderCountView(@NonNull Context context) {
         this(context, null);
@@ -42,11 +47,22 @@ public class CityHeaderCountView extends FrameLayout {
 
     public void init(Activity activity, Integer payNum, Integer guideNum) {
         this.mActivity = activity;
-        city_header_count_pay.setText(payNum.toString());
-        city_header_count_guide.setText(guideNum.toString());
+        if (payNum > 0) {
+            city_header_count_pay.setText(payNum.toString());
+            city_header_count_guide.setText(guideNum.toString());
+            if (city_header_count2_root != null) {
+                city_header_count2_root.setVisibility(View.GONE);
+            }
+        } else {
+            //隐藏玩法相关
+            if (city_header_count2_root != null) {
+                city_header_count2_root.setVisibility(View.VISIBLE);
+                city_header_count_guide2.setText(guideNum.toString());
+            }
+        }
     }
 
-    @OnClick({R.id.city_header_click1, R.id.city_header_click2})
+    @OnClick({R.id.city_header_click1, R.id.city_header_click2, R.id.city_header_count2_root})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.city_header_click1:
@@ -56,6 +72,7 @@ public class CityHeaderCountView extends FrameLayout {
 //                }
                 break;
             case R.id.city_header_click2:
+            case R.id.city_header_count2_root:
                 //中文司导事件
                 if (mActivity != null && (mActivity instanceof CityActivity)) {
                     ((CityActivity) mActivity).clickMoreGuide();
