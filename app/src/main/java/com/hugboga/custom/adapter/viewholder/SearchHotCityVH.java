2c70deed46139dbe7cf67bac4f390bf2fca93b34
zongfi <zhzephizhao@huangbaoche.com>
2017-12-21 16:54:57 +0800

    搜索界面，热门城市图片优化，增加蒙层

--- app/src/main/java/com/hugboga/custom/adapter/viewholder/SearchHotCityVH.java
+++ app/src/main/java/com/hugboga/custom/adapter/viewholder/SearchHotCityVH.java
@@ -2,12 +2,14 @@ package com.hugboga.custom.adapter.viewholder;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.hugboga.custom.R;
 import com.hugboga.custom.data.bean.DestinationHotItemBean;
 import com.hugboga.custom.utils.Tools;
+import com.hugboga.custom.utils.UIUtils;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -32,10 +34,23 @@ public class SearchHotCityVH extends RecyclerView.ViewHolder {
         itemView.setOnClickListener(onClickListener);
     }
 
-    public void init(DestinationHotItemBean bean) {
+    public void init(DestinationHotItemBean bean, int listWidth) {
         this.bean = bean;
+        resetWidthImg(listWidth); //重置图片宽和高
         Tools.showImage(search_hot_city_img, bean.destinationImageUrl, R.color.default_pic_gray);
         search_hot_city_title.setText(bean.destinationName);
+    }
+
+    /**
+     * 重置图片宽度和高度
+     * @param listWidth
+     */
+    private void resetWidthImg(int listWidth) {
+        ViewGroup.LayoutParams layoutParams = search_hot_city_img.getLayoutParams();
+        int width = (listWidth - UIUtils.dip2px(10)) / 2;
+        layoutParams.width = width;
+        layoutParams.height = width;
+        search_hot_city_img.setLayoutParams(layoutParams);
     }
 
     OnItemClickListener onItemClickListener;
