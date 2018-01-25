4f11fbcbd0795eb57d6557b8b18f8e2e73581e54
jingliang <jingliangxu@huangbaoche.com>
2018-01-25 17:23:32 +0800

    打开debug

--- app/src/main/java/com/hugboga/custom/MyApplication.java
+++ app/src/main/java/com/hugboga/custom/MyApplication.java
@@ -83,7 +83,7 @@ public class MyApplication extends HbcApplication implements Application.Activit
         initConfig();
         Log.e("hbcApplication", "debug " + BuildConfig.DEBUG);
         try {
-            CrashReport.initCrashReport(this, "900024779", false);
+//            CrashReport.initCrashReport(this, "900024779", false);
             Reservoir.init(this, 4096);
         } catch (Exception e) {
             e.printStackTrace();
