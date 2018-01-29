05be2afb69b991ff5a36b021908db3cd27b3c50e
zongfi <zhzephizhao@huangbaoche.com>
2018-01-29 18:57:24 +0800

    SKU列表显示改版

--- app/src/main/java/com/hugboga/custom/widget/city/CitySkuView.java
+++ app/src/main/java/com/hugboga/custom/widget/city/CitySkuView.java
@@ -2,16 +2,21 @@ package com.hugboga.custom.widget.city;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
+import android.text.SpannableString;
+import android.text.Spanned;
 import android.text.TextUtils;
+import android.text.style.ForegroundColorSpan;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.hugboga.custom.MainActivity;
 import com.hugboga.custom.MyApplication;
 import com.hugboga.custom.R;
 import com.hugboga.custom.activity.AiResultActivity;
@@ -21,6 +26,7 @@ import com.hugboga.custom.activity.SkuDetailActivity;
 import com.hugboga.custom.activity.WebInfoActivity;
 import com.hugboga.custom.constants.Constants;
 import com.hugboga.custom.data.bean.city.DestinationGoodsVo;
+import com.hugboga.custom.statistic.sensors.SensorsUtils;
 import com.hugboga.custom.utils.CommonUtils;
 import com.hugboga.custom.utils.PriceFormat;
 import com.hugboga.custom.utils.Tools;
@@ -59,6 +65,8 @@ public class CitySkuView extends FrameLayout implements HbcViewBehavior {
     TextView city_item_tip2; //提示语2
     @BindView(R.id.city_item_line)
     public ImageView line;
+    @BindView(R.id.city_item_favor)
+    TextView city_item_favor; //收藏数
     @BindView(R.id.city_item_hear)
     ImageView saveLineImg; //收藏线路
 
@@ -78,9 +86,9 @@ public class CitySkuView extends FrameLayout implements HbcViewBehavior {
      * 显示数据
      */
     public void init(DestinationGoodsVo destinationGoodsVo) {
-        city_item_img.getLayoutParams().height = UIUtils.getScreenWidth() - UIUtils.dip2px(20);
         this.destinationGoodsVo = destinationGoodsVo;
         Tools.showImageNotCenterCrop(city_item_img, destinationGoodsVo.goodsImageUrl, R.mipmap.home_default_route_item);
+        city_item_favor.setText(String.valueOf(destinationGoodsVo.userFavorCount));
         city_item_title.setText(destinationGoodsVo.goodsName);
         city_item_price.setText(String.format(getContext().getString(R.string.city_sku_item_price),
                 String.valueOf(PriceFormat.priceNoPoint(destinationGoodsVo.perPrice))));
@@ -96,13 +104,17 @@ public class CitySkuView extends FrameLayout implements HbcViewBehavior {
      *
      * @return
      */
-    private String getItemTitle(DestinationGoodsVo destinationGoodsVo) {
-        StringBuilder sb = new StringBuilder(String.format(getContext().getString(R.string.city_sku_title1),
-                String.valueOf(destinationGoodsVo.userFavorCount), String.valueOf(destinationGoodsVo.dayCount)));
+    private SpannableString getItemTitle(DestinationGoodsVo destinationGoodsVo) {
+        String title1 = String.format(getContext().getString(R.string.city_sku_title1),
+                String.valueOf(destinationGoodsVo.dayCount));
+        String title2 = "";
         if (!TextUtils.isEmpty(destinationGoodsVo.placeList)) {
-            sb.append(String.format(getContext().getString(R.string.city_sku_title11), destinationGoodsVo.placeList));
+            title2 = String.format(getContext().getString(R.string.city_sku_title11), destinationGoodsVo.placeList);
         }
-        return sb.toString();
+        SpannableString ssb = new SpannableString(title1 + title2);
+        ssb.setSpan(new ForegroundColorSpan(Color.parseColor("#151515")), 0, title1.length(),
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        return ssb;
     }
 
     @OnClick({R.id.city_item_hear, R.id.city_item_root_layout, R.id.city_item_guide})
@@ -126,6 +138,9 @@ public class CitySkuView extends FrameLayout implements HbcViewBehavior {
                 intent.putExtra(Constants.PARAMS_ID, destinationGoodsVo.goodsNo);
                 intent.putExtra(Constants.PARAMS_SOURCE, getEventSource());
                 getContext().startActivity(intent);
+                if (getContext() instanceof MainActivity) {
+                    SensorsUtils.onAppClick(getEventSource(), "热门专辑", "首页-热门专辑");
+                }
                 break;
             case R.id.city_item_guide:
                 if (TextUtils.isEmpty(destinationGoodsVo.guideId)) {
@@ -148,6 +163,9 @@ public class CitySkuView extends FrameLayout implements HbcViewBehavior {
         } else if (getContext() instanceof CityActivity) {
             CityActivity cityActivity = (CityActivity) getContext();
             return cityActivity.getEventSource();
+        } else if (getContext() instanceof MainActivity) {
+            MainActivity mainActivity = (MainActivity) getContext();
+            return mainActivity.getEventSource();
         }
         return null;
     }
@@ -169,8 +187,15 @@ public class CitySkuView extends FrameLayout implements HbcViewBehavior {
         if (_data instanceof DestinationGoodsVo) {
             city_item_root_layout.getLayoutParams().width = UIUtils.getScreenWidth();
             city_item_img.getLayoutParams().height = UIUtils.getScreenWidth() - UIUtils.dip2px(20);
+            city_item_img.getLayoutParams().height = UIUtils.getScreenWidth() - UIUtils.dip2px(20);
             DestinationGoodsVo bean = (DestinationGoodsVo) _data;
             init(bean);
         }
+    }
+
+    public void setDesplayViewLayoutParams(int displayImgWidth, int displayImgHeight) {
+        city_item_root_layout.getLayoutParams().width = displayImgWidth;
+        city_item_img.getLayoutParams().height = displayImgWidth;
+        city_item_img.getLayoutParams().width = displayImgWidth;
     }
 }
--- app/src/main/java/com/hugboga/custom/widget/home/HomeAlbumItemView.java
+++ app/src/main/java/com/hugboga/custom/widget/home/HomeAlbumItemView.java
@@ -1,38 +1,18 @@
 package com.hugboga.custom.widget.home;
 
 import android.content.Context;
-import android.content.Intent;
 import android.support.annotation.Nullable;
-import android.text.TextUtils;
 import android.util.AttributeSet;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
-import com.huangbaoche.hbcframe.data.net.ErrorHandler;
-import com.hugboga.custom.MyApplication;
 import com.hugboga.custom.R;
-import com.hugboga.custom.activity.GuideWebDetailActivity;
-import com.hugboga.custom.activity.SkuDetailActivity;
-import com.hugboga.custom.activity.WebInfoActivity;
-import com.hugboga.custom.constants.Constants;
 import com.hugboga.custom.data.bean.HomeBean;
-import com.hugboga.custom.data.bean.UserEntity;
-import com.hugboga.custom.statistic.sensors.SensorsUtils;
-import com.hugboga.custom.utils.CommonUtils;
-import com.hugboga.custom.utils.Tools;
-import com.hugboga.custom.utils.collection.CollectionHelper;
+import com.hugboga.custom.data.bean.city.DestinationGoodsVo;
 import com.hugboga.custom.widget.HbcViewBehavior;
-import com.sensorsdata.analytics.android.sdk.SensorsDataAPI;
-
-import net.grobas.view.PolygonImageView;
-
-import org.json.JSONObject;
+import com.hugboga.custom.widget.city.CitySkuView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 /**
  * Created by qingcha on 17/11/23.
@@ -42,28 +22,8 @@ import butterknife.OnClick;
 
 public class HomeAlbumItemView extends LinearLayout implements HbcViewBehavior {
 
-    @BindView(R.id.home_album_item_layout)
-    LinearLayout albumItemLayout;
-    @BindView(R.id.home_album_desplay_iv)
-    ImageView desplayIV;
-    @BindView(R.id.home_album_collect_iv)
-    ImageView collectIV;
-    @BindView(R.id.home_album_price_tv)
-    TextView priceTV;
-    @BindView(R.id.home_album_desplay_layout)
-    RelativeLayout desplayLayout;
-    @BindView(R.id.home_album_item_title_tv)
-    TextView titleTV;
-    @BindView(R.id.home_banner_avatar_iv)
-    PolygonImageView avatarIV;
-    @BindView(R.id.home_banner_type_tv)
-    TextView typeTV;
-    @BindView(R.id.home_banner_guide_name_tv)
-    TextView guideNameTV;
-    @BindView(R.id.home_banner_collect_tv)
-    TextView collectTV;
-    @BindView(R.id.home_banner_desc_tv)
-    TextView descTV;
+    @BindView(R.id.home_sku_view)
+    CitySkuView home_sku_view; //SKU展示item
 
     private HomeBean.AlbumBean albumBean;
 
@@ -73,16 +33,14 @@ public class HomeAlbumItemView extends LinearLayout implements HbcViewBehavior {
 
     public HomeAlbumItemView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        inflate(context, R.layout.view_home_album_item, this);
+        inflate(context, R.layout.view_home_sku_item, this);
         ButterKnife.bind(this);
-        typeTV.setText(getContext().getResources().getString(R.string.home_goodes_type1));
-        typeTV.setBackgroundColor(0xFF4A4A4A);
     }
 
     public void setDesplayViewLayoutParams(int displayImgWidth, int displayImgHeight) {
-        albumItemLayout.getLayoutParams().width = displayImgWidth;
-        desplayLayout.getLayoutParams().height = displayImgHeight;
-        desplayLayout.getLayoutParams().width = displayImgWidth;
+        if (home_sku_view != null) {
+            home_sku_view.setDesplayViewLayoutParams(displayImgWidth, displayImgHeight);
+        }
     }
 
     @Override
@@ -91,76 +49,29 @@ public class HomeAlbumItemView extends LinearLayout implements HbcViewBehavior {
             return;
         }
         albumBean = (HomeBean.AlbumBean) _data;
-        Tools.showImage(desplayIV, albumBean.goodsPic);
-        titleTV.setText(albumBean.getGoodsName());
-        priceTV.setText(getContext().getResources().getString(R.string.home_goodes_price, "" + albumBean.perPrice));
-        guideNameTV.setText(albumBean.getGuideName());
-        collectTV.setText(getContext().getResources().getString(R.string.home_goodes_favorite_num, "" + albumBean.goodsFavoriteNum));
-        descTV.setText(getContext().getResources().getString(R.string.home_goodes_service_day, "" + albumBean.goodsServiceDayNum, albumBean.routeCityDesc));
-        Tools.showImage(avatarIV, albumBean.guideAvatar, R.mipmap.icon_avatar_guide);
-
-        // 遗留代码
-        if (!UserEntity.getUser().isLogin(getContext())) {
-            collectIV.setSelected(false);
-        } else {
-            collectIV.setSelected(CollectionHelper.getIns(getContext()).getCollectionLine().isCollection(albumBean.goodsNo));
+        if (home_sku_view != null) {
+            home_sku_view.init(transSkuInfo(albumBean));
         }
     }
 
-    @OnClick(R.id.home_album_item_layout)
-    public void intentSkuDetailActivity() {
-        Intent intent = new Intent(getContext(), SkuDetailActivity.class);
-        intent.putExtra(WebInfoActivity.WEB_URL, albumBean.goodsDetailUrl);
-        intent.putExtra(Constants.PARAMS_ID, albumBean.goodsNo);
-        intent.putExtra(Constants.PARAMS_SOURCE, getEventSource());
-        getContext().startActivity(intent);
-        SensorsUtils.onAppClick(getEventSource(), "热门专辑", "首页-热门专辑");
-    }
-
-    @OnClick(R.id.home_banner_avatar_iv)
-    public void intentGuideWebDetailActivity() {
-        if (TextUtils.isEmpty(albumBean.guideId)) {
-            return;
-        }
-        GuideWebDetailActivity.Params params = new GuideWebDetailActivity.Params();
-        params.guideId = albumBean.guideId;
-        Intent intent = new Intent(getContext(), GuideWebDetailActivity.class);
-        intent.putExtra(Constants.PARAMS_DATA, params);
-        intent.putExtra(Constants.PARAMS_SOURCE, getEventSource());
-        getContext().startActivity(intent);
-    }
-
-    private String getEventSource() {
-        return "首页";
-    }
-
-    @OnClick(R.id.home_album_collect_iv)
-    public void onClickCollect() {
-        SensorsUtils.onAppClick("首页", "收藏", "");
-        if (CommonUtils.isLogin(getContext(), getEventSource())) {
-            collectIV.setEnabled(false);
-            collectIV.setSelected(!collectIV.isSelected());
-            CollectionHelper.getIns(getContext()).getCollectionLine().changeCollectionLine(albumBean.goodsNo, collectIV.isSelected());
-            if (collectIV.isSelected()) {
-                CommonUtils.showToast(getResources().getString(R.string.collect_succeed));
-                setSensorsEvent(albumBean.guideId, albumBean.goodsNo);
-            } else {
-                CommonUtils.showToast(getResources().getString(R.string.collect_cancel));
-            }
-            collectIV.setEnabled(true);
-        }
-    }
-
-    //收藏司导埋点
-    public static void setSensorsEvent(String guideId, String goodsNo) {
-        try {
-            JSONObject properties = new JSONObject();
-            properties.put("guideId", guideId);
-            properties.put("favoriteType", "司导");
-            properties.put("goodsNo", goodsNo);
-            SensorsDataAPI.sharedInstance(MyApplication.getAppContext()).track("favorite", properties);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    /**
+     * 数据转换
+     *
+     * @param albumBean
+     * @return
+     */
+    public DestinationGoodsVo transSkuInfo(HomeBean.AlbumBean albumBean) {
+        DestinationGoodsVo vo = new DestinationGoodsVo(null);
+        vo.goodsNo = albumBean.goodsNo;
+        vo.goodsImageUrl = albumBean.goodsPic;
+        vo.goodsName = albumBean.getGoodsName();
+        vo.perPrice = String.valueOf(albumBean.perPrice);
+        vo.guideHeadImageUrl = albumBean.guideAvatar;
+        vo.userFavorCount = albumBean.goodsFavoriteNum;
+        vo.dayCount = albumBean.goodsServiceDayNum;
+        vo.placeList = albumBean.routeCityDesc;
+        //FIXME XX位当地司导可带你服务，缺少字段@吕博文
+        vo.guideCount = albumBean.goodsFavoriteNum;
+        return vo;
     }
 }
